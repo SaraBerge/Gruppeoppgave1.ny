417175937cf28ea7a015302732b8211854058069
TheaBossum <thea.bossum@gmail.com>
2024-10-04 09:46:48 +0200

    Update spyderfil_oppgave_d.py
    definerer oppgavene

--- spyderfil_oppgave_d.py
+++ spyderfil_oppgave_d.py
@@ -14,7 +14,7 @@ Created on Tue Oct  1 14:12:45 2024
 
 #Oppgave d) og e)
 
-from datetime import datetime
+from datetime import datetime #e)
 
 # Åpne og les filene
 with open('temperatur_trykk_met_samme_rune_time_datasett.csv.txt', 'r') as f1, open('trykk_og_temperaturlogg_rune_time.csv.txt', 'r') as f2:
@@ -37,6 +37,7 @@ trykk_barometer_fil2 = []
 trykk_absolutt_fil2 = []
 temperatur_fil2 = []
 
+#e) 
 # Ta in dato og tid som tekst 
 # Identifisere type dato 
 # Konvertere til datetime
@@ -54,7 +55,7 @@ for linje in linjer1:
     navn_fil1.append(data[0])
     stasjon_fil1.append(data[1])
     tid_fil1.append(
-        datetime.strptime(data[2], '%d.%m.%Y %H:%M')
+        datetime.strptime(data[2], '%d.%m.%Y %H:%M')  #e)
         )  
     lufttemperatur_fil1.append(data[3])
     lufttrykk_fil1.append(data[4])
@@ -63,7 +64,7 @@ for linje in linjer1:
 for linje in linjer2:
     data = linje.strip().split(";")
     dato_tid_fil2.append(
-        parse_datetime(data[0])
+        parse_datetime(data[0])              #e)
         )
     tid_siden_start_fil2. append(data[1])
     trykk_barometer_fil2.append(data[2])
@@ -72,7 +73,8 @@ for linje in linjer2:
     
 
     
-import matplotlib.pyplot as plt
+#f)
+import matplotlib.pyplot as plt                
 
 x_koordinater = [tid_fil1, dato_tid_fil2]
 y_koordinater = [lufttemperatur_fil1, temperatur_fil2]
