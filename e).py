c6fbabf4490f1a8fb87e655955740ad6985646e2
TheaBossum <thea.bossum@gmail.com>
2024-10-04 09:28:59 +0200

    oppgave f)
    prøver meg på oppgave f

---
+++ e).py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  3 19:43:55 2024
+
+@author: theab
+"""
+
+#f)
+
+import matplotlib.pyplot as plt
+
+x_koordinater = [tid_fil1, dato_tid_fil2]
+y_koordinater = [lufttemperatur_fil1, temperatur_fil2]
