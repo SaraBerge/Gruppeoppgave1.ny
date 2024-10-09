e8eab2dc86de137a288774b096e039ab031143f4
SaraBerge <273525@uis.no>
2024-10-09 13:07:16 +0200

    FERDIG
    Her er hele oppgaven. Alt fungerer slik det skal. Dette er den helt ferdige koden som IKKE trenger å redigeres mer:)

--- prøverlitt.py
+++ prøverlitt.py
@@ -6,149 +6,171 @@ Created on Tue Oct  8 11:01:35 2024
 @author: sara
 """
 
-#Oppgave d) og e)
-from datetime import datetime
+#En del av oppgave g)
+import datetime
 import matplotlib.pyplot as plt
 
-# Åpne og les filene
-with open('temperatur_trykk_met_samme_rune_time_datasett.csv.txt', 'r') as f1, open('trykk_og_temperaturlogg_rune_time.csv.txt', 'r') as f2:
-    # Les linjene og splitt dataene på mellomrom eller komma
-    # Forutsetter at første linje er header og hopper over den
-    linjer1 = f1.readlines()[1:]
-    linjer2 = f2.readlines()[1:]
+def glidende_gjennomsnitt(tid, temperatur, n):
+    gyldige_tider = []
+    gjennomsnitt =[]
 
-# Initialiser lister for hver kolonne i fil1
-navn_fil1 = []
-stasjon_fil1 = []
-tid_fil1 = []
-lufttemperatur_fil1 = []
-lufttrykk_fil1 = []
+    for i in range(n, len(temperatur)-n):
+        temp_slice = temperatur[i - n:i + n + 1]
+        gjennomsnitt_verdi = sum(temp_slice) / len(temp_slice)
 
-# Initialiser lister for hver kolonne i fil2
-dato_tid_fil2 = []
-tid_siden_start_fil2 = []
-trykk_barometer_fil2 = []
-trykk_absolutt_fil2 = []
-temperatur_fil2 = []
+        gyldige_tider.append(tid[i])
+        gjennomsnitt.append(gjennomsnitt_verdi)
 
-#e) 
-# Ta in dato og tid som tekst 
-# Identifisere type dato 
-# Konvertere til datetime
-# Returner datetime ut av funksjonen 
-def parse_datetime(datetime_string):
-    if "/" in datetime_string:
-        if " 00:" in datetime_string:
-            datetime_string = datetime_string.replace("00:", "12:", 1)
-        return datetime.strptime(datetime_string, '%m/%d/%Y %I:%M:%S %p')
-    else:
-        return datetime.strptime(datetime_string, '%m.%d.%Y %H:%M')
+    return gyldige_tider, gjennomsnitt
 
-linjer1.pop()
+#Oppgave e)
+# Initialiser lister for hver kolonne i filene
+lufttemperatur_met = []
+tid_met = []
+lufttrykk_met = []
+lufttemperatur= []
+tid= []
+temperatur = []
+tid_bar = []
+trykk_abs = []
+trykk_bar=[]
+
 # Fyll listene for fil1
-for linje in linjer1:
-    try:
-        data = linje.strip().split(";") 
-        tid_fil1.append(
-            datetime.strptime(data[2], '%d.%m.%Y %H:%M')  #e)
-            )  
-        lufttemperatur_fil1x = data[3].replace(',', '.')
-        lufttemperatur_fil1_float = float(lufttemperatur_fil1x)
-        lufttemperatur_fil1.append(lufttemperatur_fil1_float)
-        
-        lufttrykk_fil1x = data[4].replace(",",".")
-        lufttrykk_fil1_float = float(lufttrykk_fil1x)
-        lufttrykk_fil1.append(lufttrykk_fil1_float)
-    except ValueError:
-        pass
+with open("temperatur_trykk_met_samme_rune_time_datasett.csv.txt", "r") as fil: #Åpner og leser av fil
+    for linje in fil:
+        data = linje.strip().split(";")
+        if len(data)>=5:
+            tiden=data[2]  
+            temperaturen= data[3].replace(',', '.')
+            trykk= data[4].replace(",",".")
+            try:
+                if "am" in tiden or "pm" in tiden:      #Tar hensyn til pm og am
+                    dato_obj = datetime.datetime.strptime(tiden, "%d/%m/%Y %I:%M:%S %p") 
+                else:
+                    dato_obj = datetime.datetime.strptime(tiden, "%d.%m.%Y %H:%M")
+                
+                tid_standard = dato_obj.strftime("%Y-%m-%d %H:%M:%S")
+                lufttemperatur_float= float(temperaturen)
+                lufttrykk_float= float(trykk)
+                tid_met.append(tid_standard)
+                lufttemperatur_met.append(lufttemperatur_float)
+                lufttrykk_met.append(lufttrykk_float)
+            except ValueError:
+                pass
 
 # Fyll listene for fil2
-for linje in linjer2:
-    data = linje.strip().split(";")
-    if len(data) >= 5:
-        # Hent dato og tid
-        dato_tid_fil2.append(parse_datetime(data[0]))
-        
-        # Hent verdier og erstatt komma med punktum
-        trykk_barometer_fil2x = data[2].replace(",", ".")
-        trykk_absolutt_fil2x = data[3].replace(",", ".")
-        temperatur_fil2x = data[4].replace(",", ".")
+with open("trykk_og_temperaturlogg_rune_time.csv.txt", "r") as fil:
+    for linje in fil:
+        data = linje.strip().split(";")
+        if len(data) >= 5:
+            # Hent dato og tid
+            tiden=data[0]
+            # Hent verdier og erstatt komma med punktum
+            trykk_baro= data[2].replace(",", ".")
+            trykk_abso= data[3].replace(",", ".")
+            temperaturen= data[4].replace(",", ".")
         
         # Sjekk om barometertrykk er en tom streng
-        if trykk_barometer_fil2x == '':
-            try:
-                # Hvis barometertrykk mangler, legg bare til absolutt trykk og temperatur
-                trykk_absolutt_fil2_float = float(trykk_absolutt_fil2x)
-                trykk_absolutt_fil2.append(trykk_absolutt_fil2_float)
-                
-                temperatur_fil2_float = float(temperatur_fil2x)
-                temperatur_fil2.append(temperatur_fil2_float)
-                
-                # Legg til en None-verdi for barometertrykk for å opprettholde lengden
-                trykk_barometer_fil2.append(None)
-            except ValueError:
+            if trykk_baro == (''):
+                try:
+                   if "am" in tiden or "pm" in tiden:    #Tar hensyn til pm og am
+                       if " 00:" in tiden:
+                           tiden = tiden.replace("00:", "12:", 1)
+                       dato_obj = datetime.datetime.strptime(tiden, "%m/%d/%Y %I:%M:%S %p")
+                   else:
+                       dato_obj = datetime.datetime.strptime(tiden, "%m.%d.%Y %H:%M")
+                   
+                   tid_standard = dato_obj.strftime("%Y-%m-%d %H:%M:%S")
+                   temperatur_float = float(temperaturen)
+                   trykk_abs_float = float(trykk_abso) * 10
+                   
+                   tid.append(tid_standard)
+                   temperatur.append(temperatur_float)
+                   trykk_abs.append(trykk_abs_float)
+                except ValueError:
+                    pass
+            else:
+                try:
+                    if "am" in tiden or "pm" in tiden:      #Tar hensyn til pm og am
+                       if " 00:" in tiden:
+                           tiden = tiden.replace("00:", "12:", 1)
+                       dato_obj = datetime.datetime.strptime(tiden, "%m/%d/%Y %I:%M:%S %p")
+                    else:
+                       dato_obj = datetime.datetime.strptime(tiden, "%m.%d.%Y %H:%M")
+                   
+                    tid_standard = dato_obj.strftime("%Y-%m-%d %H:%M:%S")
+                    temperatur_float=float(temperaturen)
+                    trykk_abs_float=float(trykk_abso)*10
+                    trykk_bar_float=float(trykk_baro)*10
+                   
+                    trykk_bar.append(trykk_bar_float)
+                    tid_bar.append(tid_standard)
+                    tid.append(tid_standard)
+                    temperatur.append(temperatur_float)
+                    trykk_abs.append(trykk_abs_float)
+                except ValueError:
                 # Hopp over linjer som ikke kan konverteres til float
-                pass
-        else:
-            try:
-                # Konverter og legg til barometertrykk
-                trykk_barometer_fil2_float = float(trykk_barometer_fil2x)
-                trykk_barometer_fil2.append(trykk_barometer_fil2_float)
-                
-                # Konverter og legg til absolutt trykk og temperatur
-                trykk_absolutt_fil2_float = float(trykk_absolutt_fil2x)
-                trykk_absolutt_fil2.append(trykk_absolutt_fil2_float)
-                
-                temperatur_fil2_float = float(temperatur_fil2x)
-                temperatur_fil2.append(temperatur_fil2_float)
-            except ValueError:
-                # Hopp over linjer som ikke kan konverteres til float
-                pass
+                    pass
+tider_met_dt = [datetime.datetime.strptime(tiden, "%Y-%m-%d %H:%M:%S") for tiden in tid_met]
+tider_dt = [datetime.datetime.strptime(tiden, "%Y-%m-%d %H:%M:%S") for tiden in tid]
+tider_baro_dt = [datetime.datetime.strptime(tiden, "%Y-%m-%d %H:%M:%S") for tiden in tid_bar]
 
-#f)
+#En del av oppgave g)
+n=30
+gyldige_tider, gjennomsnitt = glidende_gjennomsnitt(tider_dt, temperatur, n)
 
+#En del av oppgave h)
+start_tid = datetime.datetime(2021, 6, 11, 17, 31)
+slutt_tid = datetime.datetime(2021, 6, 12, 3, 5)
+
+temperaturer_uis_filtered = []
+tider_uis_filtered = []
+
+for tiden, temperaturen in zip(tider_dt, temperatur):
+    if start_tid <= tiden <= slutt_tid:
+        tider_uis_filtered.append(tiden)
+        temperaturer_uis_filtered.append(temperaturen)
+
+if temperaturer_uis_filtered:
+    max_temp = max(temperaturer_uis_filtered)
+    min_temp = min(temperaturer_uis_filtered)
+
+    temperaturfall_tider = [start_tid, slutt_tid]
+    temperaturfall_values = [max_temp, min_temp]
+else:
+    temperaturfall_tider = []
+    temperaturfall_values = []
+
+#Oppgave f), g), h) og i)
 # Plotting
-plt.plot(tid_fil1, lufttemperatur_fil1, label="Meterologisk")
-plt.plot(dato_tid_fil2, temperatur_fil2, label="UiS")
+plt.plot(tider_met_dt, lufttemperatur_met, label="Meterologisk")
+plt.plot(tider_dt, temperatur, label="UiS")
+plt.plot(gyldige_tider, gjennomsnitt, label="Gjennomsnittstemperatur")
+plt.plot(temperaturfall_tider, temperaturfall_values, label="Temperaturfall Maksimal til Minimal")
+plt.xlabel("Tid")
+plt.ylabel("Temperatur")
 plt.legend()
 plt.show()
 
-
-#g) 
-
-import numpy as np
-import matplotlib.pyplot as plt
-
-def beregn_gjennomsnitt(tider, temperaturer, n):
-    gyldige_tidspunkter = []
-    gjennomsnittsverdier = []
-    
-    # Gå gjennom hvert tidspunkt
-    for i in range(n, len(tider) - n):
-        # Beregn gjennomsnittet av de n forrige, den nåværende, og de n neste målingene
-        snitt = np.mean(temperaturer[i - n:i + n + 1])
-        
-        # Legg til gyldig tidspunkt og gjennomsnittstemperatur i listene
-        gyldige_tidspunkter.append(tider[i])
-        gjennomsnittsverdier.append(snitt)
-    
-    return gyldige_tidspunkter, gjennomsnittsverdier
-
-# Eksempeldata
-tider = np.linspace(0, 100, 1000)  # 1000 tidspunkter
-temperaturer = np.sin(tider) * 10 + 20  # Eksempeltemperaturer (sinuskurve)
-
-# Beregn gjennomsnitt med n=30
-n = 30
-gyldige_tidspunkter, gjennomsnittsverdier = beregn_gjennomsnitt(tider, temperaturer, n)
-
-# Plot original temperaturkurve og gjennomsnittskurve
-plt.figure(figsize=(10, 6))
-plt.plot(tider, temperaturer, label='Temperatur', color='blue')
-plt.plot(gyldige_tidspunkter, gjennomsnittsverdier, label=f'Gjennomsnitt (n={n})', color='orange')
-plt.xlabel('Tid')
-plt.ylabel('Temperatur (°C)')
-plt.title('Temperatur og glidende gjennomsnitt')
+plt.plot(tider_met_dt, lufttrykk_met, label = "Absoluttrykk MET")
+plt.plot(tider_dt, trykk_abs, label = "Absoluttrykk")
+plt.plot(tider_baro_dt, trykk_bar, label = "Barometrisk trykk")
+plt.xlabel("Tid")
+plt.ylabel("Trykk")
 plt.legend()
-plt.grid(True)
+plt.xticks(rotation=45)
+plt.tight_layout()
 plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
